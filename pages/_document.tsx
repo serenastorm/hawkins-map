21e6387c820113fb81222cea352a13831c816462
Serena Antonetti <45170586+serenastorm@users.noreply.github.com>
2023-01-08 01:22:08 +0000

    add favicon and meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,8 +15,28 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&family=Josefin+Sans:wght@700&family=Overpass+Mono:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content="/og-image.jpg" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
+        />
       </Head>
       <body>
+        {/* Canvas for particles background */}
         <div id="canvascontainer">
           <canvas id="buffer"></canvas>
           <canvas id="canvas"></canvas>
